da83d4e1c36463480503847252f1ce720d8c07ca
Kunal Kamble <kunal.kamble@eviden.com>
2023-11-30 09:32:46 +0530

    git upload script added

---
+++ gitupload.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+cd ./iflows
+#git init
+git clone -b artifacts --single-branch https://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@github.com/kunal-kamble-git/prpiper.git
+cp *.zip ./prpiper/
+cd prpiper
+git add -A
+git commit -m "artifact upload"
+git push origin artifacts
+cd ../
+rm -rf prpiper
